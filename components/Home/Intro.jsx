5b0aa2fe62c7f690245621c0860454c777028cdb
sandypockets <zac@znacreative.com>
2021-08-26 00:41:58 -0400

    Update tagline

--- components/Home/Intro.jsx
+++ components/Home/Intro.jsx
@@ -5,7 +5,7 @@ export default function Intro() {
         Blog.
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        Get a beautiful, feature-rich blog up and running. Fast.
+        When I'm not building out my next project, I'm writing about programming.
       </h4>
     </section>
   )
