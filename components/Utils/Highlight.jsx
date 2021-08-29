4d17c9bbf4fff88418b449c071bcb24b30bf690b
sandypockets <zac@znacreative.com>
2021-08-29 13:27:44 -0400

    Update highlight colours

--- components/Utils/Highlight.jsx
+++ components/Utils/Highlight.jsx
@@ -1,6 +1,6 @@
 export default function HoverHighlight({ children }) {
   return (
-    <span className="text-transparent bg-clip-text bg-gradient-to-r from-fuchsia-600 to-pink-600 dark:from-pink-600 dark:via-red-500 dark:to-yellow-500">
+    <span className="text-transparent bg-clip-text bg-gradient-to-r from-green-400 via-blue-500 to-indigo-500 dark:from-pink-700 dark:via-red-500 dark:to-yellow-500">
       {children}
     </span>
   )
--- components/Utils/HoverHighlight.jsx
+++ components/Utils/HoverHighlight.jsx
@@ -1,6 +1,6 @@
 export default function HoverHighlight({ children }) {
   return (
-    <span className="hover:text-transparent bg-clip-text bg-gradient-to-r bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500">
+    <span className="hover:text-transparent bg-clip-text bg-gradient-to-r from-green-400 via-blue-500 to-indigo-500 dark:from-pink-600 dark:via-red-500 dark:to-yellow-500">
       {children}
     </span>
   )
