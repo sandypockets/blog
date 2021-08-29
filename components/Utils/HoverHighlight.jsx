42d58d64e00fd204c090227d6b0255f40a3776ad
sandypockets <zac@znacreative.com>
2021-08-28 22:14:46 -0400

    Rename component

--- components/Utils/HoverHighlight.jsx
+++ components/Utils/HoverHighlight.jsx
@@ -1,4 +1,4 @@
-export default function Highlight({ children }) {
+export default function HoverHighlight({ children }) {
   return (
     <span className="hover:text-transparent bg-clip-text bg-gradient-to-r bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500">
       {children}
