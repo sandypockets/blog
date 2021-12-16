19eed00c467941cf5226bcefccf1132aae862bfc
sandypockets <zac@znacreative.com>
2021-12-15 23:38:48 -0500

    Fix line height

--- pages/about.jsx
+++ pages/about.jsx
@@ -14,7 +14,7 @@ export default function About() {
       </Head>
       <Container>
         <div className="pt-24 mb-12 h-full sm:pt-24 sm:my-11 sm:mx-22 md:mx-8 lg:mx-14 xl:mx-32 2xl:max-w-7xl">
-          <div className={" text-3xl pl-6 sm:text-6xl sm:leading-20 sm:pl-3 sm:mb-20 md:text-7xl md:pl-5 lg:pl-16 xl:text-8xl 2xl:text-9xl xl:pl-14 2xl:pl-16 font-bold"}>
+          <div className={" text-3xl pl-6 sm:text-6xl sm:leading-none sm:pl-3 sm:mb-20 md:text-7xl md:pl-5 lg:pl-16 xl:text-8xl 2xl:text-9xl xl:pl-14 2xl:pl-16 font-bold"}>
             <p>I'm a serial learner,{' '}
               <br/>
               helping to build a <Highlight>safe</Highlight>, <Highlight>open</Highlight>, and <Highlight>inclusive</Highlight> internet.</p>
