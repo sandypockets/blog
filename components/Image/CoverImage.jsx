6497f236065506356d06b8cd6cb9aea37827e0b2
sandypockets <zac@znacreative.com>
2022-01-08 00:37:19 -0500

    Make image grayscale

--- components/Image/CoverImage.jsx
+++ components/Image/CoverImage.jsx
@@ -1,21 +1,24 @@
 import cn from 'classnames'
 import Link from 'next/link'
 import Image from 'next/image'
+import { useState } from "react";
 
 export default function CoverImage({ title, src, slug, height, width, coverImagePriority }) {
+  const [loading, setLoading] = useState(true)
   const image = (
     <Image
       src={src}
       alt={`Cover Image for ${title}`}
       className={cn('shadow-sm object-cover', {
         'hover:shadow-md transition-shadow duration-200': slug,
-      })}
+      }, {'force-grayscale': !loading})}
       layout="responsive"
       width={width}
       height={height}
       priority={coverImagePriority ? coverImagePriority : false}
       blurDataURL={src}
       placeholder="blur"
+      onLoadingComplete={() => setLoading(false)}
     />
   )
   return (
