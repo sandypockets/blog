415a01088baa0878aa253f937ecb60667606999f
sandypockets <zac@znacreative.com>
2022-03-20 14:19:15 -0400

    Remove duplicate tag

--- components/Layout/Meta.jsx
+++ components/Layout/Meta.jsx
@@ -34,10 +34,6 @@ export default function Meta() {
       <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
       <meta name="theme-color" content="#000" />
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
-      <meta
-        name="description"
-        content={`A programming and development blog | ${BLOG_NAME}.`}
-      />
       <meta property="og:image" content={HOME_OG_IMAGE_URL} />
     </Head>
   );
